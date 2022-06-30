c06fb6a815412559888236dd5bedd55e12921598
SDozH <soldatov_alex@bk.ru>
2022-06-30 08:48:38 +0300

    Колличество пассажиров

--- ConsoleApp6Unit/Program.cs
+++ ConsoleApp6Unit/Program.cs
@@ -1,36 +1,20 @@
 ﻿using System;
 
 namespace ConsoleApp6Unit
-{
-	class Company
-	{
-		public string Type;
-		public string Name;
-	}
-
-	class Department
-	{
-		public Company Company;
-		public City City;
-	}
-
-	class City
-	{
-		public string Name;
-	}
-	class Program
-	{
-		static void Main(string[] args)
+{ 
+		class Bus
 		{
-			var department = GetCurrentDepartment();
-		}
+			public int? Load;
 
-		static Department GetCurrentDepartment()
+		public void PrintStatus()
 		{
-			if (Department?.Company?.Type == "Банк" && Department?.City?.Name == "Санкт-Перербург")
+			if (Load.HasValue)
 			{
-				Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
+				Console.WriteLine("В автобусе {0} пассажиров", Load.Value);
 			}
+			else
+			{ Console.WriteLine("Автобус пуст"); }
 		}
-	}
+		}
+
 }
