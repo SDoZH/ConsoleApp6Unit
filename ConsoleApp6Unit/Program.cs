435fefbe3b93b9d21efc2ae9403adefe4529ca29
SDozH <soldatov_alex@bk.ru>
2022-06-30 08:39:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6Unit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp6Unit
+{
+	class Company
+	{
+		public string Type;
+		public string Name;
+	}
+
+	class Department
+	{
+		public Company Company;
+		public City City;
+	}
+
+	class City
+	{
+		public string Name;
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var department = GetCurrentDepartment();
+		}
+
+		static Department GetCurrentDepartment()
+		{
+			if (Department?.Company?.Type == "Банк" && Department?.City?.Name == "Санкт-Перербург")
+			{
+				Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
+			}
+		}
+	}
+}
