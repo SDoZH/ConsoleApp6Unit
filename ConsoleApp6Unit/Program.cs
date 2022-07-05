ae4f085ed71d209d8b47cca9bb2bda8c45302b18
SDozH <soldatov_alex@bk.ru>
2022-07-05 09:05:30 +0300

    7/1/10

--- ConsoleApp6Unit/Program.cs
+++ ConsoleApp6Unit/Program.cs
@@ -4,17 +4,31 @@ namespace ConsoleApp6Unit
 {
 	class Obj
 	{
-		private string name;
-		private string owner;
-		private int length;
-		private int count;
-
-		public Obj(string name, string ownerName, int objLength, int count)
+		class BaseClass
 		{
-			this.name = name;
-			owner = ownerName;
-			length = objLength;
-			this.count = count;
+			protected string Name;
+
+			public BaseClass(string name)
+			{
+				Name = name;
+			}
+		}
+
+		class DerivedClass : BaseClass
+		{
+			public string Description;
+
+			public int Counter;
+			public DerivedClass(string name, string description) : base(name)
+			{
+				Description = description;
+			}
+			public DerivedClass(string name, string description, int counter) : base(name)
+			{
+				Description = description;
+				Counter = counter;
+			}
 		}
 	}
+	}	
 }
