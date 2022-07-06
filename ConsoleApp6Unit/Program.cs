b28dd7ee63b63582dade9ea393ae8ceb8d364b7d
SDozH <soldatov_alex@bk.ru>
2022-07-07 00:25:04 +0300

    7/2/3

--- ConsoleApp6Unit/Program.cs
+++ ConsoleApp6Unit/Program.cs
@@ -2,33 +2,20 @@
 
 namespace ConsoleApp6Unit
 {
-	class Obj
-	{
-		class BaseClass
-		{
-			protected string Name;
+    class BaseClass
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("Метод класса BaseClass");
+        }
+    }
+    class DerivedClass : BaseClass
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Метод класса DerivedClass");
+        }
 
-			public BaseClass(string name)
-			{
-				Name = name;
-			}
-		}
+    }
 
-		class DerivedClass : BaseClass
-		{
-			public string Description;
-
-			public int Counter;
-			public DerivedClass(string name, string description) : base(name)
-			{
-				Description = description;
-			}
-			public DerivedClass(string name, string description, int counter) : base(name)
-			{
-				Description = description;
-				Counter = counter;
-			}
-		}
-	}
-	}	
 }
